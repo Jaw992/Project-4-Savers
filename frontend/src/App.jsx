8f1207246b0406536e46977a5ed41f2ca894ad57
Jared Aw <jaw@Jareds-MacBook-Air.local>
2024-09-18 13:54:39 +0800

    setup frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes } from "react-router-dom";
+// import { useState } from 'react';
+// import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello</h1>
+      <Routes>
+        <Route />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
