fa41800bcf1efbca6e616871d857fde5cdb338d5
Jared Aw <jaw@Jareds-Air.singtel.com>
2024-09-20 15:34:05 +0800

    feat: setup get transactions route

--- backend/controllers/TransactionController.js
+++ backend/controllers/TransactionController.js
@@ -11,9 +11,21 @@ const pool = require("../config/db");
 */
 
 //* Get all transactions
-router.get("/", async (req, res) => {
-    const transactions = await pool.query("SELECT * FROM transactions");
-    res.status(200).json(transactions);
+router.get("/history", async (req, res) => {
+    const transactions = await pool.query('SELECT * FROM transactions');
+    res.status(200).json(transactions.rows);
+});
+
+//* Get specific transaction
+router.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    try{ 
+        const transaction = await pool.query('SELECT * FROM transactions WHERE id = $1', [id]);
+        res.status(200).json(transaction.rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error'});
+    }
 });
 
 module.exports = router;
