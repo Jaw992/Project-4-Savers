65e0c7606b881ccdddff75adc3acf7b6ddf7c453
Jared Aw <jaw@Jareds-Air.singtel.com>
2024-09-26 10:08:53 +0800

    refactor: remove conenction test to postgresql

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,12 +5,4 @@ const pool = new Pool({
     connectionString: process.env.POSTGRES_URL,
   });
 
-  pool.connect((error) => {
-    if (error) {
-      console.error("Connection error", err.stack);
-    } else {
-      console.log("Connected to PostgreSQL");
-    }
-  });
-
   module.exports = pool;
